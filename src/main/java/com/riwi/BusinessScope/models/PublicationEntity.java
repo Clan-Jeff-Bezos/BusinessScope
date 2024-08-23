265a77c0822b2a6246aa17623561b8ac3fbbc5a8
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-23 18:07:38 -0500

    Create table category

--- src/main/java/com/riwi/BusinessScope/models/PublicationEntity.java
+++ src/main/java/com/riwi/BusinessScope/models/PublicationEntity.java
@@ -1,2 +1,95 @@
-package com.riwi.BusinessScope.models;public class PublicationEntity {
+package com.riwi.BusinessScope.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+import java.sql.Date;
+
+@Entity(name = "publication")
+public class PublicationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 150,nullable = false)
+    private String title;
+    @Lob
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePublication;
+    private UserEntity idUser;
+    private CategoryEntity idCategory;
+
+    public PublicationEntity() {
+    }
+
+    public PublicationEntity(int id, String title, String description, Date datePublication, UserEntity idUser, CategoryEntity idCategory) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.datePublication = datePublication;
+        this.idUser = idUser;
+        this.idCategory = idCategory;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public UserEntity getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(UserEntity idUser) {
+        this.idUser = idUser;
+    }
+
+    public Date getDatePublication() {
+        return datePublication;
+    }
+
+    public void setDatePublication(Date datePublication) {
+        this.datePublication = datePublication;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CategoryEntity getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(CategoryEntity idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicationEntity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", datePublication=" + datePublication +
+                ", idUser=" + idUser +
+                ", idCategory=" + idCategory +
+                '}';
+    }
 }
