3b63db908e07e80af69385717bfcbce1679d9778
Jonathan <josuarezp@unal.edu.co>
2024-08-23 12:29:42 -0500

    creation of follow-up table

---
+++ src/main/java/com/riwi/BusinessScope/models/FollowUp.java
@@ -0,0 +1,28 @@
+package com.riwi.BusinessScope.models;
+
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="FollowUp")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class FollowUp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="idUser")
+    private int idUser;
+    @Column(name="followUpDate",columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime followUpDate;
+    @Column(name="idventure")
+    private int idventure;
+}
